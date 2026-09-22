66b240cf5597d579ce5a7ef1f17a59755eec0af8
Marcos <marcospontodoc@gmail.com>
2026-09-22 12:22:29 -0300

    Add tests for player creation validation scenarios

--- tests/Players/create-player.spec.ts
+++ tests/Players/create-player.spec.ts
@@ -33,3 +33,120 @@ test("should create a new player with valid data", async ({
     expect(result.rows[0].name).toBe(player.name);
     expect(result.rows[0].email).toBe(player.email);
 });
+
+test("should not create a player with invalid email", async ({
+    playerApi,
+    database
+}) => {
+
+    const player = new PlayerBuilder()
+        .withEmail("email-invalido")
+        .build();
+
+    const response = await playerApi.create(player);
+
+    expect(response.status()).toBe(400);
+
+    const result = await database.query(
+        `
+        SELECT *
+        FROM player
+        WHERE email = $1
+        `,
+        [player.email]
+    );
+
+    expect(result.rowCount).toBe(0);
+});
+
+test("should not create a player with empty email", async ({
+    playerApi,
+    database
+}) => {
+
+    const player = new PlayerBuilder()
+        .withEmail("")
+        .build();
+
+    const response = await playerApi.create(player);
+
+    expect(response.status()).toBe(400);
+
+    const result = await database.query(
+        `
+        SELECT *
+        FROM player
+        WHERE email = $1
+        `,
+        [player.email]
+    );
+
+    expect(result.rowCount).toBe(0);
+});
+
+test("should not create a player with empty name", async ({
+    playerApi,
+    database
+}) => {
+
+    const player = new PlayerBuilder()
+        .withName("")
+        .build();
+
+    const response = await playerApi.create(player);
+
+    expect(response.status()).toBe(400);
+
+    const result = await database.query(
+        `
+        SELECT *
+        FROM player
+        WHERE email = $1
+        `,
+        [player.email]
+    );
+
+    expect(result.rowCount).toBe(0);
+});
+
+test("should not create a player with existing email", async ({
+    playerApi,
+    database
+}) => {
+
+    // Create the first player
+    const existingPlayer = new PlayerBuilder().build();
+
+    const firstResponse = await playerApi.create(existingPlayer);
+
+    expect(firstResponse.status()).toBe(201);
+
+    const firstResponseBody = await firstResponse.json();
+
+    const existingPlayerId = firstResponseBody.idPlayer;
+
+  
+    const player = new PlayerBuilder()
+        .withEmail(existingPlayer.email)
+        .build();
+
+    const response = await playerApi.create(player);
+
+    expect(response.status()).toBe(400);
+
+
+    const result = await database.query(
+        `
+        SELECT *
+        FROM player
+        WHERE email = $1
+        `,
+        [existingPlayer.email]
+    );
+
+    expect(result.rowCount).toBe(1);
+
+    expect(result.rows[0].id_player).toBe(existingPlayerId);
+    expect(result.rows[0].name).toBe(existingPlayer.name);
+    expect(result.rows[0].email).toBe(existingPlayer.email);
+});
